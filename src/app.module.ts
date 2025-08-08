5f11dfad8157d4cc933045f435c6f32ff14f5eff
João Gustavo <joaogustavosilva2001@gmail.com>
2025-08-08 07:32:41 -0300

    chore: configure `ConfigModule` and `TypeOrmModule` in `AppModule`
    - Enabled global `.env` configuration using `ConfigModule.forRoot`
    - Added `TypeOrmModule.forRootAsync` to load PostgreSQL connection settings from environment variables
    - Registered `Order` entity and enabled schema synchronization

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,36 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config'; // <-- importe aqui
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrdersModule } from './orders/orders.module';
 import { KafkaModule } from './kafka/kafka.module';
 import { ElasticsearchModule } from './elasticsearch/elasticsearch.module';
 import { SharedModule } from './shared/shared.module';
+import { Order } from './orders/entities/order.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [OrdersModule, KafkaModule, ElasticsearchModule, SharedModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // importa variáveis do .env globalmente
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: 'postgres',
+        port: config.get<number>('POSTGRES_PORT'),
+        username: config.get<string>('POSTGRES_USER'),
+        password: config.get<string>('POSTGRES_PASSWORD'),
+        database: config.get<string>('POSTGRES_DB'),
+        entities: [Order],
+        synchronize: true,
+      }),
+    }),
+    OrdersModule,
+    KafkaModule,
+    ElasticsearchModule,
+    SharedModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
